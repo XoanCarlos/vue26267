bb9851fcf4e41118c737ff5dd092a4740794ad5e
Juan Carlos Pérez González <jcarlos@lnxd-i2e31.teis.intranet>
2026-09-17 12:48:48 +0200

    mejoras diseño xestion pacientes

--- src/components/XestionPacientes.vue
+++ src/components/XestionPacientes.vue
@@ -5,7 +5,17 @@
       <div class="fila">
         <div class="campo campo-dni">
           <label>DNI/CIF:</label>
-          <input v-model="novoPaciente.dni" type="text" required style="text-align: center;" />
+          <input
+            v-model="novoPaciente.dni"
+            type="text"
+            required
+            style="text-align: center"
+            @blur="validarDni"
+            :class="{ 'is-invalid': !dniValido }"
+          />
+        </div>
+        <div v-if="!dniValido" class="invalid-texto d-block">
+          DNI o NIE inválido.
         </div>
         <div class="campo campo-nome">
           <label>Nome:</label>
@@ -23,7 +33,7 @@
         </div>
         <div class="campo campo-correo">
           <label>Correo:</label>
-          <input v-model="novoPaciente.correo" type="email" required />
+          <input v-model="novoPaciente.correo" type="email" />
         </div>
         <div class="campo campo-movil">
           <label>Móvil:</label>
@@ -34,9 +44,13 @@
         <div class="campo campo-direccion">
           <label>Direccion:</label>
           <input v-model="novoPaciente.direccion" type="text" required />
-        </div>       
+        </div>
       </div>
-      <button type="submit" class="btn-guardar" :disabled="novoPaciente.dni === '' || novoPaciente.nome === ''">
+      <button
+        type="submit"
+        class="btn-guardar"
+        :disabled="novoPaciente.dni === '' || novoPaciente.nome === ''"
+      >
         Gardar
       </button>
     </form>
@@ -57,13 +71,13 @@
       <tbody>
         <tr v-for="(u, index) in usuarios" :key="index">
           <td>{{ index + 1 }}</td>
-          <td style="text-align: center;">{{ u.dni }}</td>
+          <td style="text-align: center">{{ u.dni }}</td>
           <td>{{ u.nome }}</td>
           <td>{{ u.correo }}</td>
           <td>{{ u.provincia }}</td>
-          <td style="text-align: center;">{{ u.activo ? "✅" : "❌" }}</td>
+          <td style="text-align: center">{{ u.activo ? "✅" : "❌" }}</td>
           <td>{{ u.tipoCuenta }}</td>
-          <td style="text-align: center;">
+          <td style="text-align: center">
             <button @click="editarUsuario(index)" title="Editar">✏️</button>
             <button @click="eliminarUsuario(index)" title="Eliminar">🗑️</button>
           </td>
@@ -77,9 +91,9 @@
 
 <script setup>
 /// Zona de declaracións
-import { ref, reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted } from "vue";
 
-const usuarios = ref([])  //almacena la lista de usuarios e os seus cambios
+const usuarios = ref([]); //almacena la lista de usuarios e os seus cambios
 
 const novoPaciente = reactive({
   dni: "",
@@ -87,36 +101,98 @@ const novoPaciente = reactive({
   correo: "",
   provincia: "",
   activo: false,
-  tipoCuenta: ""
-})
+  tipoCuenta: "",
+});
 
 /// Zona de ciclo de vida
 
-onMounted(() => {       //sempre se cargan estos usuarios de exemplo ao iniciar o componente
+onMounted(() => {
+  //sempre se cargan estos usuarios de exemplo ao iniciar o componente
   usuarios.value = [
-    { dni: "A000000C", nome: "Soldaduras SL", correo: "soldadura@email.com", provincia: "A Coruña", activo: true, tipoCuenta: "empresa" },
-    { dni: "0000000C", nome: "María Pérez", correo: "maria@email.com", provincia: "Lugo", activo: false, tipoCuenta: "particular" },
-    { dni: "B1234567D", nome: "Xosé López", correo: "xose@email.com", provincia: "Ourense", activo: true, tipoCuenta: "particular" },
-    { dni: "C9876543E", nome: "Construcións Modernas", correo: "construcion@email.com", provincia: "Pontevedra", activo: true, tipoCuenta: "empresa" }
-  ]
-})
+    {
+      dni: "A000000C",
+      nome: "Soldaduras SL",
+      correo: "soldadura@email.com",
+      provincia: "A Coruña",
+      activo: true,
+      tipoCuenta: "empresa",
+    },
+    {
+      dni: "0000000C",
+      nome: "María Pérez",
+      correo: "maria@email.com",
+      provincia: "Lugo",
+      activo: false,
+      tipoCuenta: "particular",
+    },
+    {
+      dni: "B1234567D",
+      nome: "Xosé López",
+      correo: "xose@email.com",
+      provincia: "Ourense",
+      activo: true,
+      tipoCuenta: "particular",
+    },
+    {
+      dni: "C9876543E",
+      nome: "Construcións Modernas",
+      correo: "construcion@email.com",
+      provincia: "Pontevedra",
+      activo: true,
+      tipoCuenta: "empresa",
+    },
+  ];
+});
 
-/// Zona de métodos ou funcións
+/// Zona de métodos ou funcións bbdd
 
 function gardarUsuario() {
-  usuarios.value.push({ ...novoPaciente })  //engade o novo usuario á lista (copia do obxecto)
+  usuarios.value.push({ ...novoPaciente }); //engade o novo usuario á lista (copia do obxecto)
   //Object.assign(novoPaciente, { dni: "", nome: "", correo: "", provincia: "", activo: false, tipoCuenta: "" }) //reinicia o formulario
 }
 
 function eliminarUsuario(index) {
-  usuarios.value.splice(index, 1);   //elimina o usuario da lista
+  usuarios.value.splice(index, 1); //elimina o usuario da lista
 }
 
 function editarUsuario(index) {
-  const usuario = usuarios.value[index];   //carga os datos do usuario elixido no formulario
-  Object.assign(novoPaciente, usuario);  // carga os datos do usuario no formulario recorda v-model do formulario é novoPaciente
+  const usuario = usuarios.value[index]; //carga os datos do usuario elixido no formulario
+  Object.assign(novoPaciente, usuario); // carga os datos do usuario no formulario recorda v-model do formulario é novoPaciente
 }
 
+//  =====================================================
+// funciones auxiliares
+
+// Estado de validez del DNI/NIE si la estructura de datos es más compleja se usa reactive
+const dniValido = ref(true); // Por defecto es válido y no muestra error al iniciar
+
+// Función para validar DNI y NIE
+const validarDniNie = (valor) => {
+  const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+  const dniRegex = /^[0-9]{8}[A-Z]$/;
+  const nieRegex = /^[XYZ][0-9]{7}[A-Z]$/;
+
+  valor = valor.toUpperCase();
+
+  if (dniRegex.test(valor)) {
+    const numero = parseInt(valor.slice(0, 8), 10);
+    const letra = valor.charAt(8);
+    return letra === letras[numero % 23]; //sale con true si es válido
+  } else if (nieRegex.test(valor)) {
+    const nie = valor.replace("X", "0").replace("Y", "1").replace("Z", "2");
+    const numero = parseInt(nie.slice(0, 8), 10);
+    const letra = valor.charAt(8);
+    return letra === letras[numero % 23]; //sale con true si es válido
+  }
+  return false;
+};
+
+// Validar al salir del campo
+const validarDni = () => {
+  novoPaciente.dni = novoPaciente.dni.trim().toUpperCase();
+  dniValido.value = validarDniNie(novoPaciente.dni);
+  // Actualiza el estado de validez
+};
 </script>
 
 <style scoped>
@@ -158,7 +234,7 @@ form {
 }
 
 .campo-dni {
-  flex:  1;
+  flex: 1;
   /* ocupa menos espacio */
   border-radius: 0px;
 }
@@ -204,7 +280,7 @@ form {
   min-width: 80px;
   /* ancho fijo para alinear */
   font-weight: 500;
-  font: bold
+  font: bold;
 }
 
 .campo input {
@@ -220,7 +296,7 @@ form {
   background-color: #f0f3f7;
   color: black;
   border: 3;
-  border-color:#1bb191;
+  border-color: #1bb191;
   padding: 0.4rem 1.5rem;
   border-radius: 0px;
   cursor: pointer;
@@ -272,6 +348,18 @@ h4 {
   font-weight: 600;
   background-color: #1bb191;
   color: white;
+}
+
+.is-invalid {
+  border-color: #f28b82 !important;
+  background-color: #ffe6e6;
+ }
+
+.invalid-texto {
+  display: block;
+  font-size: 10px;
+  color: red;
+  padding-top: 0.5%;
 }
 
 @media (max-width: 768px) {
